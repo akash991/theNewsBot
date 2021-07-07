12a130b860395392adde4b2c29d08901175f916b
akash991 <soni.akash991@gmail.com>
2021-07-08 03:06:19 +0530

    Reset number of articles read wherever required

--- theNewsBot/telebot/messages.py
+++ theNewsBot/telebot/messages.py
@@ -1,6 +1,6 @@
 START_MESSAGE = "Hello!! I'm theNewsBot, your one-stop source for daily news.\n\nMy creator is not much of a talker and unfortunately, so am I. Press on /yes and allow me to show you everything I've got."
-BEGIN = "Great.\nNow select from the following categories available for you.\n\n"
-SUBCATEGORIES_BEGIN = "Please select from the following sub-categories\n\n"
+BEGIN = "Great.\nNow select any one of the following categories\n\n"
+SUBCATEGORIES_BEGIN = "Select from the following sub-categories\n\n"
 STOP_MESSAGE = "Thanks for your time. Have a great day"
 SOURCE = "\n/source , select another source"
 NEXT = "\n/next , navigate to the next set"
